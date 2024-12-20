851d21a2891bff9f2e5cbd58954a1d07f7ed8e0b
Ameer Osman Yousufzia <ameerosmanyousufzia@gmail.com>
2024-12-20 21:36:49 +0000

    Form Submission, access form data
    Create a form in the html where you pass in a YT url, we print this url out in the console. After form submission, we redirect back to root page. Next plan is to create a web scraping tool that will get the YT video transcript.

--- web/routes.py
+++ web/routes.py
@@ -1,6 +1,13 @@
-from flask import render_template
+from flask import Flask, render_template, redirect, request
 
 def init_routes(app):
     @app.route("/", methods=["GET", "POST"])
     def index():
         return render_template("index.html")
+
+    @app.route("/url_submission", methods=["POST"])
+    def url_submission_route():
+        if request.method == "POST":
+            yt_url = request.form.get("yt_url")
+            print(yt_url)
+            return redirect("/")a
