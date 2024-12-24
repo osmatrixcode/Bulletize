b95dd35f7302a3b9c7592bb6fd6d2e160cbfbe2c
Ameer Osman Yousufzia <ameerosmanyousufzia@gmail.com>
2024-12-24 16:59:08 +0000

    Successfully output YT Video Transcript on Console
    Managed to execute a script, that will return the transcript of the youtube video. I am not using headless mode at the moment, so I can further develop with Selenium with ease. I am planning to now somehow get the transcript from the console, into the python application.

--- utils/youtube_scraper.py
+++ utils/youtube_scraper.py
@@ -4,22 +4,29 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import time
 
-get_transcript_script="const transcript = [...document.querySelectorAll('.segment-text')].map(e => e.textContent).join(' '); console.log(transcript);"
 open_more_script = "const showMoreButton = document.querySelector('tp-yt-paper-button#expand'); if (showMoreButton) {showMoreButton.click();} else {console.log('Show more button not found.');}"
 open_transcript_box_script = "document.querySelector('button[aria-label=\\'Show transcript\\']').click();"
+get_transcript_script="const transcript = [...document.querySelectorAll('.segment-text')].map(e => e.textContent).join(' '); console.log(transcript);"
 
 
 def ytTranscript(url):
     options = webdriver.ChromeOptions()
-    options.add_argument("--headless=new")
+    # options.add_argument("--headless=new")
     driver = webdriver.Chrome(options=options)
     
     driver.get(url)
+    
     time.sleep(5)
-    print("Successful Headless Mode!")
     driver.execute_script(open_more_script)
+    print("opened the 'more' section")
+    
     time.sleep(2)
     driver.execute_script(open_transcript_box_script)
-    # driver.execute_script("const transcript = [...document.querySelectorAll('.segment-text')].map(e => e.textContent).join(' '); console.log(transcript);")
-    print("ytTranscript function running!")
+    print("opened the 'transcript' box")
+    
+    time.sleep(2)
+    transcript = driver.execute_script(get_transcript_script)
+    print("outputted the actual transcript")
+    print(transcript)
+    
     # driver.quit()
