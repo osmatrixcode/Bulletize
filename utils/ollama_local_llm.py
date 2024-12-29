4f856cd578701410964bce7d7f2430b6ce4f2307
Ameer Osman Yousufzia <ameerosmanyousufzia@gmail.com>
2024-12-29 17:35:42 +0000

    Update ollama_local_llm.py
    Use Response Streaming, to see output of Local LLM to my terminal. I am also using llama3.2 since it is a lower size model, and produce faster responses compared to llama3.1. Next plan is to see if we can pass in a .txt file to analyse

--- utils/ollama_local_llm.py
+++ utils/ollama_local_llm.py
@@ -1,12 +1,24 @@
 from ollama import chat
 from ollama import ChatResponse
 
-response: ChatResponse = chat(model='llama3.1', messages=[
-  {
-    'role': 'user',
-    'content': 'Why is the sky blue?',
-  },
-])
-print(response['message']['content'])
-# or access fields directly from the response object
-print(response.message.content)
+# try:
+#     response: ChatResponse = chat(model='llama3.2', messages=[
+#         {
+#             'role': 'user',
+#             'content': 'Why is the sky blue?',
+#         },
+#     ])
+#     print(response['message']['content'])
+#     print(response.message.content)
+# except Exception as e:
+#     print(f"Error: {e}")
+
+
+stream = chat(
+    model='llama3.2',
+    messages=[{'role': 'user', 'content': 'Why is the sky blue?'}],
+    stream=True,
+)
+
+for chunk in stream:
+  print(chunk['message']['content'], end='', flush=True)
