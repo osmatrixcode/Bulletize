f2b70c41f030083c22fb250c7de85d2109e950fb
Ameer Osman Yousufzia <ameerosmanyousufzia@gmail.com>
2024-12-29 17:56:28 +0000

    Local LLM Analyse YT Transcript
    Made the local LLM model to analyse the transcript by: opening the transcript, storing the contents into a variable, pass it into the local LLM to produce a response. I am thinking if we even need the .txt file, as we can pass the string straight to this python file (if we make this file into a function, and pass the function into youtube_scrapper.py)

--- utils/ollama_local_llm.py
+++ utils/ollama_local_llm.py
@@ -1,5 +1,6 @@
 from ollama import chat
 from ollama import ChatResponse
+import os
 
 # try:
 #     response: ChatResponse = chat(model='llama3.2', messages=[
@@ -13,10 +14,18 @@ from ollama import ChatResponse
 # except Exception as e:
 #     print(f"Error: {e}")
 
+filename = "yt_transcript.txt"
+file_path = os.path.join("..","transcripts",filename)
+
+with open(file_path, "r") as file:
+  content = file.read()
+
+
+
 
 stream = chat(
     model='llama3.2',
-    messages=[{'role': 'user', 'content': 'Why is the sky blue?'}],
+    messages=[{'role': 'user', 'content': f'Summarise this text from top to bottom, into one paragraph: {content}'}],
     stream=True,
 )
 
